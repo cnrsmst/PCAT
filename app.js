3776bb0b7cc7e4954584b56c3ec26cde40ed723b
Caner <cnr.smst@hotmail.com>
2023-02-13 13:21:58 +0300

    static file uploaded

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');   
+
+
+app.use(express.static('public'));
+app.get('/',(req,res) => {
+
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(` Sunucu ${port} Port'unda çalışmaya başladı`);
+});
